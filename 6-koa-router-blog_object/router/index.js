7da99133dda7785e98e8ee7a023da574a9f1bbfb
Wesley Tsai <wesleyboy42@gmail.com>
2016-06-01 12:47:58 +0800

    Add example to explain the simple blog structure with object method.

---
+++ 6-koa-router-blog_object/router/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var Router = require('koa-router');
+var router = module.exports = new Router();
+var Bloger = require('../lib/blog');
+
+router.get('/', function *() {
+	var text = 	yield Bloger.init();
+	var articles = 	yield Bloger.articles();
+	
+	console.log(text);
+	console.log(articles);
+	
+	this.body = articles;
+});
---
+++ 6-koa-router-blog_object/router/users.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var Router = require('koa-router');
+var router = module.exports = new Router();
+var Bloger = require('../lib/blog');
+
+router.get('/register', function *() {
+	this.body = 'register page.';
+});
+
+router.get('/signin', function *() {
+    this.body = 'signin page.';
+})
+
+router.get('/signout', function *() {
+	this.body = 'signout page.';
+});
+
+router.get('/forget', function *() {
+    this.body = 'forget page.';
+})
+
+router.get('/profile/', function *() {
+	var articles = yield Bloger.GetArticles();
+	this.body = articles;
+});
+
+router.get('/article/', function *() {
+	this.body = 'Add article by user.';
+})
+
+router.get('/article/modify/:ArticleId', function *() {
+	var article = yield Bloger.GetArticle(this.params.ArticleId);
+	this.body = article + ' ' +this.params.ArticleId + ' by user.';
+});
+
+router.get('/article/comment/:ArticleId', function *() {
+	var article = 	yield Bloger.GetArticle(this.params.ArticleId);
+	var Comments = 	yield Bloger.Comments(this.params.ArticleId);
+	
+	this.body = article + '\n and \n' + Comments;
+})
